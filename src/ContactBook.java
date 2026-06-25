9a57ea194f303592335b8ccb4ddcc6e4f40359cd
Aaron Chen <ache0210@student.monash.edu>
2026-06-25 18:04:06 +1000

    Added loading animations using thread.sleep and "\r" + for loops

--- src/ContactBook.java
+++ src/ContactBook.java
@@ -56,5 +56,7 @@ public class ContactBook {
         return contactList;
     }
 
+    
+
 
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,41 +1,127 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
+import java.lang.Thread;
 
 public class Menu {
 
-private static boolean isRunning = true;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        Menu menu = new Menu();
         ContactBook contactBook = new ContactBook();
 
-        while (isRunning) {
-            runMainloop();
-        }
+        menu.runMainloop(contactBook);
     }
 
-    public static void runMainloop() {
-        Scanner scanner = new Scanner(System.in);
+    public void runMainloop(ContactBook contactBook) throws InterruptedException {
 
-        System.out.println("1. Add Contact");
-        System.out.println("2. Display Saved Contacts");
-        System.out.println("3. Search Contact");
-        System.out.println("4. Remove Contact");
-        System.out.println("5. Exit");
+        boolean loadingScreen = false;
 
-        int userInput = scanner.nextInt();
+        boolean isRunning = true;
 
-        switch (userInput) {
+        while (isRunning) {
+            Scanner scanner = new Scanner(System.in);
 
-            case 1:
-                System.out.print("Contact name: ");
-            case 2:
-                System.out.print("Displaying saved contacts: ");
-            case 3:
-                System.out.print("Contact name: ");
-            case 4:
-                System.out.print("Contact name: ");
-            case 5:
-                System.out.print("Contact name: ");
+
+
+            if (!loadingScreen)
+
+            {
+
+                System.out.print("Loading...");
+
+
+                Thread.sleep(500);
+
+
+
+                for (int i = 0 ; i <= 100 ; i++) {
+                    System.out.print("\r" + "=".repeat(i) + ">" + i + "%");
+                    Thread.sleep(25);
+                }
+
+
+                System.out.println();
+                loadingScreen = true;
+            }
+
+
+
+            System.out.println("1. Add Contact");
+            System.out.println("2. Display Saved Contacts");
+            System.out.println("3. Search Contact");
+            System.out.println("4. Remove Contact");
+            System.out.println("5. Exit");
+
+            int userInput = scanner.nextInt();
+
+            scanner.nextLine();
+
+            switch (userInput) {
+
+                // Add Contact
+                case 1:
+
+                    ArrayList<String> contactDetails = new ArrayList<>();
+
+                    System.out.print("Contact name: ");
+                    String name = scanner.nextLine();
+
+                    contactDetails.add(name);
+
+
+                    System.out.print("Contact Address: ");
+                    String address = scanner.nextLine();
+
+                    contactDetails.add(address);
+
+
+                    System.out.print("Favourite food: ");
+                    String food = scanner.nextLine();
+
+                    contactDetails.add(food);
+
+
+                    contactBook.addContact(name, contactDetails);
+                    System.out.println("Contact (" + name + ") succesfully added.");
+                    break; // exit loop to allow for another option
+
+
+                // Display all contacts
+                case 2:
+                    System.out.print("Displaying saved contacts: ");
+
+                // Search contact
+                case 3:
+                    System.out.print("Contact name: ");
+
+                // Remove Contact
+                case 4:
+                    System.out.print("Contact name: ");
+
+                // Exit out of interface
+                case 5:
+                    System.out.print("Exiting interface...");
+
+                    Random random = new Random();
+
+
+                    Thread.sleep(300);
+                    for (int i = 0 ; i <= 100 ; i++) {
+                        System.out.print("\r" + "=".repeat(i) + ">" + i + "%");
+                        Thread.sleep(100);
+                    }
+
+
+                    isRunning = false;
+                    break;
+
+
+                default:
+                    System.out.println("Invalid number bro");
+            }
+
         }
 
     }
